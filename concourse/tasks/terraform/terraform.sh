6965f20f53be42612fbb0c25e08cd1872c8f3be0
Glenn Bech <glenn.bech@gmail.com>
2018-09-23 23:32:36 +0200

    revert

--- concourse/tasks/terraform/terraform.sh
+++ concourse/tasks/terraform/terraform.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-export GITHIB_ACCESS_TOKEN=$github_token
-cd source
-
 terraform init
-terraform apply -refresh=true -auto-approve=true
+terraform apply -refresh=true -auto-approve=true -lock-timeout=$lock_timeout
 # Fails if there is no output (which is not really a failure)
 set +e
 terraform output -json > ${DIR}/terraform/output.json
