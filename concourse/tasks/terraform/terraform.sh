2e5db7c384fa0f454b4789eaf12cf7285f8f2d41
Glenn Bech <glenn.bech@gmail.com>
2018-09-23 23:27:48 +0200

    terrafomr refactor

--- concourse/tasks/terraform/terraform.sh
+++ concourse/tasks/terraform/terraform.sh
@@ -2,7 +2,7 @@
 
 cd source
 terraform init
-terraform apply -refresh=true -auto-approve=true -lock-timeout=$lock_timeout
+terraform apply -refresh=true -auto-approve=true 
 # Fails if there is no output (which is not really a failure)
 set +e
 terraform output -json > ${DIR}/terraform/output.json
