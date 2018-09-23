3b7d6ed2177aae64becf92523dedf8c68671bf75
Glenn Bech <glenn.bech@gmail.com>
2018-09-23 23:30:36 +0200

    terrafomr refactor

--- concourse/tasks/terraform/terraform.sh
+++ concourse/tasks/terraform/terraform.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+export GITHIB_ACCESS_TOKEN=$github_token
 cd source
+
 terraform init
 terraform apply -refresh=true -auto-approve=true
 # Fails if there is no output (which is not really a failure)
