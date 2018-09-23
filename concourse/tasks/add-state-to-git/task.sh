fadebcd03ef3b3b58d223a4b3899c7293b433760
Glenn Bech <glenn.bech@gmail.com>
2018-09-24 00:03:06 +0200

    changes

--- concourse/tasks/add-state-to-git/task.sh
+++ concourse/tasks/add-state-to-git/task.sh
@@ -2,5 +2,10 @@
 
 export GITHUB_TOKEN="${github_token}"
 cd source
+
+git config --global user.email "concourse-ci@localhost"
+git config --global user.name "concourse-ci"
+git status
+
 git add terraform.tfstate
 git commit -m"state change"
