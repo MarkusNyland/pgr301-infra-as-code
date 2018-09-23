24a8f1c8ab2b6b7b1d73c43c1f32af7c942a3396
Glenn Bech <glenn.bech@gmail.com>
2018-09-24 00:13:10 +0200

    copy source

--- concourse/tasks/add-state-to-git/task.sh
+++ concourse/tasks/add-state-to-git/task.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+find .
+DIR="${PWD}"
 export GITHUB_TOKEN="${github_token}"
 cd source
 
@@ -9,3 +11,9 @@ git status
 
 git add terraform.tfstate
 git commit -m"state change"
+
+git config --global user.email "concourse-ci@localhost"
+git config --global user.name "concourse-ci"
+git status
+
+git clone "${DIR}/source" "${DIR}/modified-source"
