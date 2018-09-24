b309b47d7ce390c23e4bab1f20e269c3c88824e8
Glenn Bech <glenn.bech@gmail.com>
2018-09-24 06:51:10 +0200

    testing

--- concourse/tasks/terraform/checkin.sh
+++ concourse/tasks/terraform/checkin.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 find .
+git add sorce/terraform.tfstate
+git config --global user.email "concourse-ci@localhost"
+git config --global user.name "concourse-ci"
+git status
 
+set +e
+git commit -m ":airplane: Terraform state."
+set -e
