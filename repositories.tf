8caa8f63860cf5e61d496e2eefeed69b68881df5
Glenn Bech <glenn.bech@gmail.com>
2018-09-28 21:47:59 +0200

    new repo

--- repositories.tf
+++ repositories.tf
@@ -3,3 +3,9 @@ resource "github_repository" "concourse_heroku" {
   description = "Shows heroku with concourse"
   private     = false
 }
+
+resource "github_repository" "concourse_heroku" {
+  name        = "heroku_example-app"
+  description = "Shows heroku with concourse"
+  private     = false
+}
